ebe9f5544759e494b16a890506cb2aa2f6567f28
Pranavan Theivendiram <234rana@gmail.com>
2014-03-11 18:23:22 +0530

    Merge origin/master
    Conflicts:
    	src/db/ui/PassengerWindow.java

--- src/db/dao/DriverDAO.java
+++ src/db/dao/DriverDAO.java
@@ -8,7 +8,14 @@ package db.dao;
 
 import db.entity.Driver;
 import db.entity.DriverId;
+import db.util.HibernateUtil;
+import java.util.List;
+import java.util.Vector;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -20,6 +27,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class DriverDAO {
     
+      private static String QUERY_BASED_ON_Name = "from drivers a where a.name like '";
     
     public static boolean addData(Integer id,String name,String address,String tp_no){
         Session sess = null;
@@ -54,5 +62,48 @@ public class DriverDAO {
     
     public static boolean updateData(){
         return true;
+    }
+    
+
+    public static JTable runQueryBasedOnName(String name) {
+       return executeHQLQuery(QUERY_BASED_ON_Name +"'"+ name + "%'");
+    }
+
+    private static JTable executeHQLQuery(String hql) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query q = session.createQuery(hql);
+            java.util.List resultList = q.list();
+            JTable jTable =displayResult(resultList);
+            session.getTransaction().commit();
+            return jTable;
+        } catch (HibernateException he) {
+            he.printStackTrace();
+        }
+        return null;
+    }
+
+
+    private static JTable displayResult(List resultList) {
+        Vector<String> tableHeaders = new Vector<String>();
+        Vector tableData = new Vector();
+        tableHeaders.add("Id");
+        tableHeaders.add("Name");
+        tableHeaders.add("Address");
+        tableHeaders.add("TelephoneNumber");
+
+        for (Object o : resultList) {
+            Driver driver = (Driver) o;
+            Vector<Object> oneRow = new Vector<Object>();
+            oneRow.add(driver.getId().getId());
+            oneRow.add(driver.getId().getName());
+            oneRow.add(driver.getAddress());
+            oneRow.add(driver.getTpNo());
+            tableData.add(oneRow);
+        }
+        JTable jTable=null;
+        jTable.setModel(new DefaultTableModel(tableData, tableHeaders));
+        return jTable;
     }
 }
