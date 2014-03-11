1a79e2e7e80aa305768b6445f460417748958328
Gobinath <Gobinath@Gobinath-PC>
2014-03-11 20:20:03 +0530

    Fixed error handling

--- src/db/ui/panels/TownEditPanel.java
+++ src/db/ui/panels/TownEditPanel.java
@@ -32,11 +32,11 @@ public class TownEditPanel extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 683, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 465, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
