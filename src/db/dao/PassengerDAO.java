c3fb951b1e6f9173570e69636ea7e138f390199f
Vishnuvathsasarma <Vishnuvathsasarma@10.10.34.197>
2014-03-11 15:13:02 +0530

    PassengerDAO

--- src/db/dao/PassengerDAO.java
+++ src/db/dao/PassengerDAO.java
@@ -72,7 +72,49 @@ public class PassengerDAO {
         }
         Transaction transaction = null;
         try {
-            String HQLQuery = "FROM Town t ";
+            String HQLQuery = "FROM Passenger p ";
+            Query query = session.createQuery(HQLQuery);
+            List<Passenger> towns = query.list();
+            return towns;
+        } catch (HibernateException | HeadlessException ex) {
+            if (transaction != null && transaction.wasCommitted()) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return null;
+    }
+
+    public List<Passenger> getPassenger(String name) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        if (session == null) {
+            return null;
+        }
+        Transaction transaction = null;
+        try {
+            String HQLQuery = "FROM Passenger p WHERE p.name= "+name;
+            Query query = session.createQuery(HQLQuery);
+            List<Passenger> towns = query.list();
+            return towns;
+        } catch (HibernateException | HeadlessException ex) {
+            if (transaction != null && transaction.wasCommitted()) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return null;
+    }
+
+    public List<Passenger> getPassenger(int ID) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        if (session == null) {
+            return null;
+        }
+        Transaction transaction = null;
+        try {
+            String HQLQuery = "FROM Passenger p WHERE p.id= "+ID;
             Query query = session.createQuery(HQLQuery);
             List<Passenger> towns = query.list();
             return towns;
